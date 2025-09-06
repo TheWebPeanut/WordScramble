0b15d1c9baf9c6ad7b3af8cbdf69ffa4966b813b
Emilio Elenes Praget <emilio.elenespraget@fressnapf.com>
2025-09-06 13:03:42 +0200

    Initial Commit

---
+++ WordScramble/WordScrambleApp.swift
@@ -0,0 +1,17 @@
+//
+//  WordScrambleApp.swift
+//  WordScramble
+//
+//  Created by Elenes Praget, Emilio on 06.09.25.
+//
+
+import SwiftUI
+
+@main
+struct WordScrambleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
