76c894eb0d18709c4e044136d0cfcaea7d63eac2
Emilio Elenes Praget <emilio.elenespraget@fressnapf.com>
2025-09-06 13:59:52 +0200

    Text checker

--- WordScramble/ContentView.swift
+++ WordScramble/ContentView.swift
@@ -8,26 +8,18 @@
 import SwiftUI
 
 struct ContentView: View {
-    let people = ["Finn", "Leia", "Luke", "Rey"]
     
     var body: some View {
-        List {
-            Text("Static Row")
-            
-            ForEach(people, id: \.self) {
-                Text($0)
-            }
-            
-            Text("Static Row")
-        }
+        
     }
-    
-    func testBundles() {
-        if let fileURL = Bundle.main.url(forResource: "somefile", withExtension: "txt") {
-            if let fileContents = try? String(contentsOf: fileURL) {
-                // then we do something with the fileURL
-            }
-        }
+    func testString() {
+        let word = "swift"
+        let checker = UITextChecker()
+        
+        let range = NSRange(location: 0, length: word.utf16.count)
+        let misspelledRange = checker.rangeOfMisspelledWord(in: word, range: range, startingAt: 0, wrap: false, language: "en")
+        
+        let allGood = misspelledRange.location == NSNotFound
     }
 }
 
